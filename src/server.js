598f26db8fb6770a39e8d91d6246c6d0c1293184
GytisKupinas <gytis.kupinas@gmail.com>
2021-08-05 00:28:42 +0300

    update

--- src/server.js
+++ src/server.js
@@ -4,6 +4,12 @@ const app = express();
 const port = 3000;
 
 app.use(express.static("web"));
+
+app.get("/suma", (req, res) => {
+    const sk1 = parseFloat(req.query.pirmas);
+    const sk2 = parseFloat(req.query.antras);
+    res.send(sk1 + sk2 + "");
+});
  
  
 // app.get("/labas", (req, res) => {
